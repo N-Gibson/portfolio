a67b4d6e593df6b4f7f1e6aaad604c17b6f14062
N-Gibson <noah.gibson99@gmail.com>
2022-05-31 07:13:24 -0600

    Rearrange dependencies for compiler

--- src/@portfolio/Components/Nav/index.tsx
+++ src/@portfolio/Components/Nav/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Nav = () => {
-  return <div>Nav</div>
+  return (
+    <div>
+      <h2>Noah Gibson</h2>
+      <h2>Home</h2>
+      <h2>About</h2>
+      <h2>Resume</h2>
+      <h2>Portfolio</h2>
+    </div>
+  )
 }
 
 export { Nav }
